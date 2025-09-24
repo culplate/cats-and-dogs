3046242862e4ce5026782486a2d95f87306965cb
Vlad <105807855+culplate@users.noreply.github.com>
2025-09-24 11:58:22 +0200

    fix(lint): changed "'"  to &apos; for sustanability

--- src/app/breed/[id]/not-found.tsx
+++ src/app/breed/[id]/not-found.tsx
@@ -8,8 +8,8 @@ export default function NotFound() {
           Breed Not Found
         </h1>
         <p className="text-gray-600 mb-6">
-          Sorry, we couldn't find the breed you're looking for. It may have been
-          removed or the link might be incorrect.
+          Sorry, we couldn&apos;t find the breed you&apos;re looking for. It may
+          have been removed or the link might be incorrect.
         </p>
 
         <BackButton />
